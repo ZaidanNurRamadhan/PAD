fe5b58e739f914e9cbd0b42b81bddd699d3c502e
FarhanPerdana123 <muhammadfarhanperdana@ugm.ac.id>
2024-11-29 16:25:25 +0700

    Perubahan BE 29/11/2024

--- app/Http/Controllers/auth/LoginController.php
+++ app/Http/Controllers/auth/LoginController.php
@@ -19,7 +19,6 @@ class LoginController extends Controller
    /**
      * Handle the login request and redirect based on role.
      */
-<<<<<<< HEAD
     public function login(Request $request)
 {
     $credentials = $request->validate([
@@ -37,39 +36,14 @@ class LoginController extends Controller
             return redirect()->route('dashboard');
         } elseif ($user->role === 'karyawan') {
             return redirect()->route('transaksi-karyawan');
-=======
-    public function store(Request $request)
-    {
-        // Validate the login credentials
-        $credentials = $request->validate([
-            'name' => 'required',
-            'password' => 'required',
-        ]);
-
-        // Attempt to authenticate and login the user
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-            
-            // Redirect based on user role
-            $user = Auth::user();
-            if ($user->role === 'owner') {
-                return redirect()->route('dashboard');
-            } elseif ($user->role === 'karyawan') {
-                return redirect()->route('gudang-karyawan');
-            }
->>>>>>> 6ce463685d1ac55b573310d3257e08c1b7aa3930
         }
     }
 
-<<<<<<< HEAD
     return back()->withErrors([
         'name' => 'The provided credentials do not match our records.',
     ])->onlyInput('name');
 }
 
-
-=======
->>>>>>> 6ce463685d1ac55b573310d3257e08c1b7aa3930
     /**
      * Logout the user and redirect to the login page.
      */
